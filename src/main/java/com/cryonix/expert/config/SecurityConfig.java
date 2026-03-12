e859a7c793da7f86f7625605a9538964725115b7
Saurabh Biswal <saurabh.biswal@example.com>
2026-03-12 16:00:27 +0530

    chore: enabled JWT authorization button in Swagger UI

--- src/main/java/com/cryonix/expert/config/SecurityConfig.java
+++ src/main/java/com/cryonix/expert/config/SecurityConfig.java
@@ -27,7 +27,7 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**", "/login/**", "/oauth2/**", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .requestMatchers("/api/auth/**", "/login/**", "/oauth2/**", "/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session
