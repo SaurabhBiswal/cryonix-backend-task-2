99e18c9b33ddca01c16c6fdd76c1c2be562e31dd
Saurabh Biswal <saurabh.biswal@example.com>
2026-03-12 22:49:36 +0530

    feat: added local .env support and dotenv-java dependency

--- src/main/java/com/cryonix/expert/ExpertServiceApplication.java
+++ src/main/java/com/cryonix/expert/ExpertServiceApplication.java
@@ -3,10 +3,20 @@ package com.cryonix.expert;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class ExpertServiceApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure()
+				.ignoreIfMissing()
+				.load();
+		
+		dotenv.entries().forEach(entry -> {
+			System.setProperty(entry.getKey(), entry.getValue());
+		});
+
 		SpringApplication.run(ExpertServiceApplication.class, args);
 	}
 
