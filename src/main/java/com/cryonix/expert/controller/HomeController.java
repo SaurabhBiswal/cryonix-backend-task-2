5e034d949f8515fc953bc38eb209deed19592fb1
Saurabh Biswal <saurabh.biswal@example.com>
2026-03-13 17:13:30 +0530

    refactor: direct redirect from root to google login

--- src/main/java/com/cryonix/expert/controller/HomeController.java
+++ src/main/java/com/cryonix/expert/controller/HomeController.java
@@ -8,6 +8,6 @@ public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "redirect:/experts";
+        return "redirect:/oauth2/authorization/google";
     }
 }
